3b4e88d66d8f44abc63cf1867061bd87ac936169
Suwashshrestha <suwashshrestha5536@gmail.com>
2024-06-15 22:28:38 +0545

    just change

---
+++ react_EduHungry/app/src/App.jsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react'
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link,
+// } from 'react-router-dom'
+import img from './img/VECTOR ART IMAGE.png'
+// import Home from './component/home.js'
+// import about from './component/about.js'
+// import contact from './component/contact.js'
+import Navbar from './component/navbar/navbar.jsx'
+import './App.css'
+
+function App(){
+    return(
+       <div>
+            <Navbar />
+            <div className="bg-indigo-600 w-full h-screen"></div>
+
+       </div>
+
+    )
+   
+}
+
+export default App
